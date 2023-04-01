554c0f40b5698ab7260927e3a4a9828c4e7706e6
DeveloperJavaMain <115426650+DeveloperJavaMain@users.noreply.github.com>
2023-04-01 23:39:54 +0300

    Db
    Db

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -29,12 +29,12 @@ public class UserService {
         return storage.getById(id);
     }
 
-    public User insert(@Valid User user) {
+    public User insert(User user) {
         validator.validate(user);
         return storage.insert(user);
     }
 
-    public User update(@Valid User user) {
+    public User update(User user) {
         validator.validate(user);
         return storage.update(user);
     }
@@ -48,7 +48,7 @@ public class UserService {
         User user = getById(userId);
         User friend = getById(friendId);
         user.getFriends().add(friendId);
-        friend.getFriends().add(userId);
+        update(user);
     }
 
     public void remFriend(int userId, int friendId) {
@@ -56,6 +56,7 @@ public class UserService {
         User friend = getById(friendId);
         user.getFriends().remove(friendId);
         friend.getFriends().remove(userId);
+        update(user);
     }
 
     public List<User> commonFriends(int userId, int friendId) {
