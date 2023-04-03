e945b2291d7362d585086ed261996a4803da9f09
DeveloperJavaMain <115426650+DeveloperJavaMain@users.noreply.github.com>
2023-04-03 19:44:17 +0300

    Add_NotBlank
    Add NotBlank to Genre and MpaRate

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class Genre implements Comparable<Genre> {
     private int id;
+    @NotBlank
     private String name;
 
     @Override
